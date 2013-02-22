ea5f2fe5bd37b6887b949fd2bb150a0b9b2ea458
Robb Schiller <robb.schiller@gmail.com>
2013-02-22 17:18:03 -0500

    updated readme

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(sass.middleware({
+      src: __dirname
+    , dest: __dirname + '/public'
+    , debug: true
+  }));
   app.use(express.static(__dirname + '/public'));
 });
 
