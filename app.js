3339525a47c3cc558021b50bd33f9138beb50c1b
Robb Schiller <robb.schiller@gmail.com>
2013-02-22 13:04:39 -0500

    trying to fix app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
+var express = require("express")
+  , routes = require('./routes');
 
-var app = module.exports = express.createServer();
+var app = express();
 
 // Configuration
 
@@ -31,9 +30,3 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
