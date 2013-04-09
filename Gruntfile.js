63c041b975e10fb98c5fd34f7e5a4d0f82b71478
Robb Schiller <robb.schiller@gmail.com>
2013-04-09 13:12:06 -0400

    added compass, jshint tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
 	// Project Config
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			all: ['Gruntfile.js', 'app/scripts/main.js']
+		},
 		sass: {
 			dist: {
 				compass: true,
@@ -10,14 +13,31 @@ module.exports = function(grunt) {
 				trace: true,
 				files: {
 					'./dist/styles/main.css' : 'app/styles/main.scss'
-				},
-			},
+				}
+			}
 		},
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'app/styles',
+					cssDir: 'dist/styles',
+					environment: 'production'
+				}
+			},
+			dev: {
+				options: {
+					sassDir: 'app/styles',
+					cssDir: 'dist/styles'
+				}
+			}
+		}
 	});
 
 	// Load the plugins
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-compass');
 
 	// Default tasks
-	grunt.registerTask('default', ['sass']);
+	grunt.registerTask('default', ['jshint', 'sass', 'compass']);
 };
