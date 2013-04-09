3682823a41e1f2e45070e37d0297d4556a267ffd
Robb Schiller <robb.schiller@gmail.com>
2013-04-09 12:36:37 -0400

    added grunt-sass task, compiles to /dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,23 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-sass');
+
+	// Project Config
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		sass: {
 			dist: {
 				compass: true,
 				loadPath: 'app/styles',
-				trace; true,
+				trace: true,
 				files: {
-					'app/styles/main.css' : 'app/styles/main.scss'
+					'./dist/styles/main.css' : 'app/styles/main.scss'
 				},
 			},
 		},
 	});
+
+	// Load the plugins
+	grunt.loadNpmTasks('grunt-contrib-sass');
+
+	// Default tasks
+	grunt.registerTask('default', ['sass']);
 };
